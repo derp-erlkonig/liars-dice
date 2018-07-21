3892ba7ad7aec6a33c0d5b569d482bf7ceceac54
allen <allen@DESKTOP-UMAG7V3>
2018-07-20 20:49:33 -0700

    Testing to see if we have git functionality.

---
+++ liars dice/src/liars/dice/LiarsDice.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package liars.dice;
+
+/**
+ *
+ * @author allen
+ */
+public class LiarsDice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tylerisabigoletiddy();
+        // TODO code application logic here
+    }
+
+    private static void tylerisabigoletiddy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
