0eb52dbe80fef7b7d62ebe304aae39652671c4d6
Allen <allen@DESKTOP-UMAG7V3>
2018-07-20 22:06:17 -0700

    Server made to accept clients. Client made to try and connect to server. No real data transfer functions have been implemented.

--- liars dice/src/liars/dice/LiarsDice.java
+++ liars dice/src/liars/dice/LiarsDice.java
@@ -1,31 +1,13 @@
 /*
- * The MIT License
- *
- * Copyright 2018 Allen Raab.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package liars.dice;
 
 /**
  *
- * @author Allen Raab
+ * @author allen
  */
 public class LiarsDice {
 
@@ -33,8 +15,8 @@ public class LiarsDice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Server serv = new Server(7777, 2);
-        Client clie = new Client("10.0.0.1", 9999);
+        //need to run them in separate threads
+        Server serv = new Server(7777, 2);
+        Client client = new Client("10.0.0.1",7777);
     }
-    
 }
